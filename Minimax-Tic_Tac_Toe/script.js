38d56e66c6c6b5ac8d764c5f6c473ad4e538097e
Prashant patel <76206077+Praahant@users.noreply.github.com>
2024-02-20 14:25:26 +0530

    basic template

---
+++ Minimax-Tic_Tac_Toe/script.js
@@ -0,0 +1,18 @@
+
+var board;
+const human='O';
+const Ai='X';
+
+
+const winCombos = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [6, 4, 2]
+]
+const cell=document.querySelectorAll('.cell');
+startgame()
